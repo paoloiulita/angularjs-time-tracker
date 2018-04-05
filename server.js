e4f74f741c1f05399e7a7da1e5cc05bd57fe00a0
Paolo Iulita <paolo.iulita@gmail.com>
2018-04-05 22:46:18 +0200

    Add Access-Control-Allow-Origin to server response

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Expires', '0');
   next();
 });
