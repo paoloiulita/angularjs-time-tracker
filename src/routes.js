1f2b434b55a87543879c0200e18528248467e4a4
Paolo Iulita <paolo.iulita@gmail.com>
2018-04-05 20:20:53 +0200

    Change indentation to tabs

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@ export default routesConfig;
 
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-  $locationProvider.html5Mode(true).hashPrefix('!');
-  $urlRouterProvider.otherwise('/');
+	$locationProvider.html5Mode(true).hashPrefix('!');
+	$urlRouterProvider.otherwise('/');
 
-  $stateProvider
-    .state('app', {
-      url: '/',
-      component: 'timerDashboard'
-    });
+	$stateProvider
+		.state('app', {
+			url: '/',
+			component: 'timerDashboard'
+		});
 }
