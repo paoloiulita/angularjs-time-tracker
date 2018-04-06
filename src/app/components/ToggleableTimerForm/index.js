9f830137ba751fdfcf2510b28ea03ac6b10be6e3
Paolo Iulita <paolo.iulita@gmail.com>
2018-04-06 19:51:20 +0200

    Add logic to togglable timer form

--- src/app/components/ToggleableTimerForm/index.js
+++ src/app/components/ToggleableTimerForm/index.js
@@ -1,6 +1,12 @@
 export const ToggleableTimerForm = {
 	template: require('./tpl.html'),
 	controller() {
-	//
+		this.$onInit = () => {
+			this.showForm = false;
+		};
+
+		this.dismiss = () => {
+			this.showForm = false;
+		};
 	}
 };
