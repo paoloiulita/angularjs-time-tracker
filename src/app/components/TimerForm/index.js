49f16c007a08ac9708879a353e9975ba34143766
Paolo Iulita <paolo.iulita@gmail.com>
2018-04-06 19:51:31 +0200

    Timer form is able to save

--- src/app/components/TimerForm/index.js
+++ src/app/components/TimerForm/index.js
@@ -1,6 +1,33 @@
+import v4 from 'uuid/v4';
+
 export const TimerForm = {
 	template: require('./tpl.html'),
-	controller() {
-	//
+	bindings: {
+		isEditing: '<',
+		dismiss: '='
+	},
+	controller(timerService) {
+		this.isLoading = false;
+
+		const doCreate = () => {
+			return timerService.createTimer({
+				title: this.title,
+				project: this.project,
+				id: v4()
+			});
+		};
+
+		const doEdit = () => {
+
+		};
+
+		this.submitForm = () => {
+			this.isLoading = true;
+			const promise = this.isEditing ? doEdit : doCreate;
+			promise().then(() => {
+				this.isLoading = false;
+				this.dismiss();
+			});
+		};
 	}
 };
