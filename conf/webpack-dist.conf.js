398706eca74c0f207da4382b7bb98b559c4ac6ff
Paolo Iulita <paolo.iulita@gmail.com>
2018-04-17 22:13:42 +0200

    Add file loader

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -43,6 +43,10 @@ module.exports = {
         loaders: [
           'html-loader'
         ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -43,10 +43,10 @@ module.exports = {
           'html-loader'
         ]
       },
-	  {
-		test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-		loader: 'url-loader?limit=100000'
-	  }
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
     ]
   },
   plugins: [
